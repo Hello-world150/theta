ce81a5d10610ef3a8c4fa11055d1c1edd07ed9ab
Haolin Qin <qhl314159265@126.com>
2024-08-13 12:10:42 +0800

    vga输出Hello Theta

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // 不链接 Rust 标准库
+#![no_main] // 禁用所有 Rust 层级的入口点
+
+use core::panic::PanicInfo;
+
+#[no_mangle] // 不重整函数名
+pub extern "C" fn _start() -> ! {
+    // 入口点函数 
+    loop {}
+}
+
+/// 入口点
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
