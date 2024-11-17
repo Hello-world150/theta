1169e0211b324d8382d1a64e101a02a193785e16
Haolin Qin <qhl314159265@126.com>
2024-11-17 16:38:56 +0800

    实现write宏

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,15 @@
 #![no_main] //禁用所有 Rust 层级的入口点
 
 use core::panic::PanicInfo;
-mod vga_buffer;
 
-static HELLO: &[u8] = b"Hello Theta!"; //声明字节形式的字符串`Hello Theta!`
-const VGA_START_POINT:u32 = 0xb8000; //VGA映射内存起点
+use vga_buffer::print_something;
+mod vga_buffer;
 
 //入口函数
 #[no_mangle] //不重整函数名
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = VGA_START_POINT as *mut u8; //将VGA内存起点转化为裸指针
-
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; //偏移指针并写入`0xb`(青色)
-        }
-    }
-
-    //永不返回 
+    print_something();
+    //永不返回
     loop {}
 }
 
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,12 +1,12 @@
 //实现VGA字符输出
-
-use volatile::Volatile;
 use core::fmt;
+use volatile::Volatile;
 
 #[allow(dead_code)] //忽略无效代码
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] //自动实现trait
 #[repr(u8)] //以u8形式存储枚举类型
-pub enum Color {
+
+pub enum Colors {
     Black = 0,
     Blue = 1,
     Green = 2,
@@ -26,72 +26,68 @@ pub enum Color {
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] //自动实现trait
-#[repr(transparent)]
-struct ColorCode(u8); //封装单个字符颜色
+#[repr(transparent)] //使用u8相同内存布局
+struct Color(u8); //单个字符颜色
 
-impl ColorCode {
-    //实现new方法
-#[warn(dead_code)]
-    fn new(foreground: Color, background: Color) -> ColorCode {
-        ColorCode((background as u8) << 4 | (foreground as u8))
+#[allow(dead_code)]
+impl Color {
+    fn new(foreground: Colors, background: Colors) -> Color {
+        //实现new方法
+        Color((background as u8) << 4 | (foreground as u8))
     }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)] //自动实现trait
 #[repr(C)] //使用C语言内存布局
-struct ScreenChar { //封装单个字符
+struct Char {
+    //封装单个字符
     ascii_character: u8,
-    color_code: ColorCode,
+    color: Color,
 }
 
+//缓冲区
+
+pub const VGA_START_POINT: u32 = 0xb8000; //VGA内存起点
 const BUFFER_HEIGHT: usize = 25; //设置缓冲区最大高度
 const BUFFER_WIDTH: usize = 80; //设置缓冲区最大宽度
 
-#[repr(transparent)]
-struct Buffer { //抽象VGA缓冲区
-    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_HEIGHT],
+#[repr(transparent)] //使用相同内存布局
+struct Buffer {
+    //VGA缓冲区
+    chars: [[Volatile<Char>; BUFFER_WIDTH]; BUFFER_HEIGHT],
 }
 
-//用于打印字符的结构体
 pub struct Writer {
-    column_position: usize,
-    color_code: ColorCode,
+    column_position: usize, //光标位置
+    color: Color,
     buffer: &'static mut Buffer, //全局有效的VGA缓冲区可变借用
 }
 
-//实现打印方法
 impl Writer {
     pub fn write_byte(&mut self, byte: u8) {
+        //按字节打印
         match byte {
-            b'\n' => self.new_line(),
+            b'\n' => self.new_line(), //立即换行
             byte => {
                 if self.column_position >= BUFFER_WIDTH {
-                    self.new_line();
+                    self.new_line(); //当光标超出缓冲区宽度换行
                 }
-                
-                let row = BUFFER_HEIGHT - 1;
-                let col = self.column_position;
 
-                let color_code = self.color_code;
-                self.buffer.chars[row][col].write(ScreenChar {
+                let row = BUFFER_HEIGHT - 1;
+                let column = self.column_position; //更新光标位置
+
+                let color = self.color;
+                self.buffer.chars[row][column].write(Char {
                     ascii_character: byte,
-                    color_code,
+                    color,
                 });
                 self.column_position += 1;
             }
         }
     }
 
-    fn new_line(&mut self) {
-        for row in 1..BUFFER_HEIGHT {
-            for col in 0..BUFFER_WIDTH {
-                let _character = self.buffer.chars[row][col].read();
-
-            }
-        }
-    }
-
     pub fn write_string(&mut self, s: &str) {
+        //按字符串打印
         for byte in s.bytes() {
             match byte {
                 //可打印字符或\n
@@ -101,12 +97,33 @@ impl Writer {
             }
         }
     }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let _character = self.buffer.chars[row][col].read();
+            }
+        }
+    }
 }
 
-//实现格式化宏
+//覆写内置Write宏
 impl fmt::Write for Writer {
     fn write_str(&mut self, s: &str) -> fmt::Result {
         self.write_string(s);
         Ok(())
     }
+}
+
+pub fn print_something() {
+    use core::fmt::Write;
+    let mut writer = Writer {
+        column_position: 0,
+        color: Color::new(Colors::Yellow, Colors::Black),
+        buffer: unsafe { &mut *(VGA_START_POINT as *mut Buffer) },
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ello! ");
+    write!(writer, "The numbers are {} and {}", 42, 1.0 / 3.0).unwrap();
 }
